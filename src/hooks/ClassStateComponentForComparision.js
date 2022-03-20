b5c8c7d144f1fe16e2a317b132b8a26625346d55
Bharathwaj Vasudevan <Bharathwaj.Vasudevan@agilysys.com>
2022-03-20 23:59:42 +0530

    course/react-hooks/learn/lecture/12727925#notes

--- src/hooks/ClassStateComponentForComparision.js
+++ src/hooks/ClassStateComponentForComparision.js
@@ -5,7 +5,9 @@ import React, { Component } from "react"
 export default class ClassStateComponentForComparision extends Component {
     state = {
         count: 1,
-        isOn: false
+        isOn: false,
+        x:null,
+        y:null
     }
     Increment = () => {
         // this.setState({
@@ -24,11 +26,20 @@ export default class ClassStateComponentForComparision extends Component {
     //lifecycle methords
     componentDidMount(){  //on initialized call in class component 
         document.title =  this.state.count ;
+        window.addEventListener('mousemove',this.handleMove); // eg: to add event listner 
     }
     componentDidUpdate(){  //for each component update
         document.title =  this.state.count ;
     }
-
+    componentWillUnmount(){  //when component detaches - can remove not needed things which might cause memory leaks
+        window.removeEventListener('mousemove',this.handleMove); 
+    }
+    handleMove= event =>{
+        this.setState({
+            x: event.pageX,
+            y: event.pageY
+        });
+    }
     render() {
         return (<>
             <h2>counter </h2>
@@ -36,6 +47,10 @@ export default class ClassStateComponentForComparision extends Component {
 
             <h2>toggle Lights </h2>
             <div style={{ height: '50px', width: '50px', background: this.state.isOn ? "yellow" :  "grey" }} onClick={this.ToggleLight}></div>
+
+            <h2>Mouse position using event listner </h2>
+            <p>x position {this.state.x}</p>
+            <p>y position {this.state.y}</p>
         </>
         )
     }
